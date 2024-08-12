37f2b266ce655a5217ae77e12aca021867fd7622
Sargamjain813 <141730944+Sargamjain813@users.noreply.github.com>
2024-08-13 00:12:10 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: School/Remove vowels from string/remove-vowels-from-string.java
@@ -0,0 +1,41 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            String s = read.readLine();
+            
+            Solution ob = new Solution();
+            String result = ob.removeVowels(s);
+            
+            System.out.println(result);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    String removeVowels(String S) {
+        StringBuilder sting = new StringBuilder();
+        for(int i = 0; i < S.length(); i ++){
+            char currentChar = S.charAt(i);
+            if(currentChar != 'a' && currentChar != 'e' && currentChar != 'i' && currentChar != 'o' && currentChar != 'u' ){
+            sting.append(currentChar);
+            }
+
+    
+     }
+
+ return sting.toString();
+    }
+}
