f7c2d3e4af723272e6cff36e219bcaaa34b8176a
Sargamjain813 <141730944+Sargamjain813@users.noreply.github.com>
2024-09-08 15:49:54 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Minimum Jumps/minimum-jumps.java
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+            String line = br.readLine();
+            String[] tokens = line.split(" ");
+
+            // Create an ArrayList to store the integers
+            ArrayList<Integer> array = new ArrayList<>();
+
+            // Parse the tokens into integers and add to the array
+            for (String token : tokens) {
+                array.add(Integer.parseInt(token));
+            }
+
+            int[] arr = new int[array.size()];
+            int idx = 0;
+            for (int i : array) arr[idx++] = i;
+
+            System.out.println(new Solution().minJumps(arr));
+            // System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    static int minJumps(int[] arr) {
+        int n=arr.length;
+        int x=0,y=0,z=0;
+        for(int i=0;i<n-1;i++){
+            x=Math.max(x,arr[i]+i);
+            if(i==y){
+                y=x;
+                z++;
+            }
+        }
+        if(y>=n-1)return z;
+        return -1;
+    }
+}
