64d0cd0b0897e2983979e9ea8d6e6195c26e1bc2
BlazeFury <141730944+sargam-jain@users.noreply.github.com>
2025-01-31 15:40:08 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Node at a given index in linked list/node-at-a-given-index-in-linked-list.java
@@ -0,0 +1,86 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int x) {
+        data = x;
+        next = null;
+    }
+}
+
+public class LinkedList {
+    public static void printList(Node node) {
+        while (node != null) {
+            System.out.print(node.data + " ");
+            node = node.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+            ArrayList<Integer> arr = new ArrayList<>();
+            String input = br.readLine();
+            StringTokenizer st = new StringTokenizer(input);
+            while (st.hasMoreTokens()) {
+                arr.add(Integer.parseInt(st.nextToken()));
+            }
+            int k = Integer.parseInt(br.readLine());
+            Node head = new Node(arr.get(0));
+            Node tail = head;
+            for (int i = 1; i < arr.size(); ++i) {
+                tail.next = new Node(arr.get(i));
+                tail = tail.next;
+            }
+            Solution ob = new Solution();
+            int res = ob.GetNth(head, k);
+            System.out.println(res);
+        
+System.out.println("~");
+}
+    }
+}
+// } Driver Code Ends
+
+
+/*node class of the linked list
+class Node
+{
+    int data;
+    Node next;
+    Node(int key)
+    {
+        data = key;
+        next = null;
+    }
+}
+*/
+
+class Solution {
+    public int GetNth(Node head, int index) {
+        // Code here
+        Node curr = head;
+        int len = 0;
+        while(curr != null){
+            len++;
+            curr = curr.next;
+        }
+        if(len<index){
+            return -1;
+        }
+        Node current = head;
+        for(int i=0;i<len;i++){
+            if(i+1 == index){
+                return current.data;
+            }
+            current = current.next;
+        }
+        return -1;
+    }
+}
