556005bf7baeade0381f5c0da33dadd61e23003c
BlazeFury <141730944+sargam-jain@users.noreply.github.com>
2025-03-27 23:52:04 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum Platforms/minimum-platforms.java
+++ Difficulty: Medium/Minimum Platforms/minimum-platforms.java
@@ -44,24 +44,25 @@ class Solution {
     // Function to find the minimum number of platforms required at the
     // railway station such that no train waits.
     static int findPlatform(int arr[], int dep[]) {
-        int n = arr.length;
+        // add your code here
         Arrays.sort(arr);
         Arrays.sort(dep);
-        
-        int i = 0;
-        int j=0;
-        int platform = 0;
-        int maxPlat = 0;
-        while(i<n && j<n){
-            if(arr[i]<= dep[j]){
-                platform++;
+
+        int n=arr.length;
+        int maxplatform=0;
+        int platformneeded=0;
+        int i=0,j=0;
+        while(i<n &&j<n){
+            if(arr[i]<=dep[j]){
+                platformneeded++;
                 i++;
             }else{
-                platform--;
+                platformneeded--;
                 j++;
             }
-            maxPlat = Math.max(platform,maxPlat);
+            
+            maxplatform=Math.max(maxplatform,platformneeded);
         }
-        return maxPlat;
+        return maxplatform;
     }
 }
