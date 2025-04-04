64fa406e0d6860fe664ea62d72f449abfbe086e4
BlazeFury <141730944+sargam-jain@users.noreply.github.com>
2025-04-04 23:45:48 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.java
+++ Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.java
@@ -1,62 +1,63 @@
 //{ Driver Code Starts
+// Initial Template for Java
 import java.io.*;
-import java.lang.*;
 import java.util.*;
 
 class GFG {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine().trim());
-        while (T-- > 0) {
-            String[] s = br.readLine().trim().split(" ");
-            int V = Integer.parseInt(s[0]);
-            int E = Integer.parseInt(s[1]);
-            ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-            for (int i = 0; i < V; i++) adj.add(i, new ArrayList<Integer>());
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int tc = sc.nextInt();
+        while (tc-- > 0) {
+            int V = sc.nextInt();
+            int E = sc.nextInt();
+            int[][] edges = new int[E][2];
             for (int i = 0; i < E; i++) {
-                String[] S = br.readLine().trim().split(" ");
-                int u = Integer.parseInt(S[0]);
-                int v = Integer.parseInt(S[1]);
-                adj.get(u).add(v);
-                adj.get(v).add(u);
+                edges[i][0] = sc.nextInt();
+                edges[i][1] = sc.nextInt();
             }
-            Solution obj = new Solution();
-            boolean ans = obj.isCycle(adj);
-            if (ans)
-                System.out.println("1");
-            else
-                System.out.println("0");
 
+            Solution obj = new Solution();
+            boolean ans = obj.isCycle(V, edges);
+            System.out.println(ans ? "true" : "false");
             System.out.println("~");
         }
+        sc.close();
     }
 }
+
 // } Driver Code Ends
 
 
 class Solution {
-    public boolean dfs(ArrayList<ArrayList<Integer>> graph, int curr, int parent,boolean[] visited){
-        visited[curr] = true;
-        
-        for(int i : graph.get(curr)){
-            if(visited[i] && i!= parent){
-                return true;
-            }
-            if(!visited[i]){
-                if(dfs(graph,i,curr,visited)) return true;
+    public boolean isCycle(int V, int[][] edges) {
+        // Code here
+        int n = V;
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < V; i++) {
+            adj.add(new ArrayList<>());
+        }
+
+        // Fill the adjacency list with the edges
+        for (int[] edge : edges) {
+            adj.get(edge[0]).add(edge[1]);
+            adj.get(edge[1]).add(edge[0]);
+        }
+        boolean [] visit = new boolean[n];
+        for(int i=0;i<n;i++){
+            if(!visit[i]){
+                if(bfs(i,adj,visit,-1)) return true; 
             }
         }
         return false;
     }
-    // Function to detect cycle in an undirected graph.
-    public boolean isCycle(ArrayList<ArrayList<Integer>> adj) {
-        boolean[] visited = new boolean[adj.size()];
-        for(int i=0;i<visited.length;i++){
-            if(!visited[i]){
-                boolean ans = dfs(adj,i,-1,visited);
-                if(ans == true) return true;
-                
+    
+    public boolean bfs(int s,ArrayList<ArrayList<Integer>> adj,boolean [] visit,int parent){
+        visit[s] = true;
+        for(int elem:adj.get(s)){
+            if(!visit[elem]){
+                if(bfs(elem,adj,visit,s)) return true;
             }
+            else if(elem!=parent) return true;
         }
         return false;
     }
